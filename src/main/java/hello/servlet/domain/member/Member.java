493d8365bd9db6a2acb0f5ab3b421dd9cc4c3058
Keunoh <rmsdhcjswo1@gmail.com>
2022-12-22 23:39:20 +0900

    비즈니스 로직은 좋으나 html을 자바로 생성해야함

--- src/main/java/hello/servlet/domain/member/Member.java
+++ src/main/java/hello/servlet/domain/member/Member.java
@@ -3,7 +3,9 @@ package hello.servlet.domain.member;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
+@ToString
 @Getter @Setter
 @NoArgsConstructor
 public class Member {
